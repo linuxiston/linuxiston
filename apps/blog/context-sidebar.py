f4c657bb9343e298472ad8fe4861b5b1ef8b7682
sevbo2003 <sevbofx@gmail.com>
2022-04-05 18:28:38 +0500

    Sidebar updated

---
+++ apps/blog/context-sidebar.py
@@ -0,0 +1,9 @@
+from .models import Category, Post, VideoPost
+
+
+def sidebar(request):
+    return {
+        'sidebar_cats': Category.objects.all(),
+        'sidebar_posts': Post.objects.all()[:4],
+        'sidebar_video_posts': VideoPost.objects.all()[:4],
+    }
