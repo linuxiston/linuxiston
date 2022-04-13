28bb6ecdf62f7ebfe0e730b26e351d8412887290
sevbo2003 <sevbofx@gmail.com>
2022-04-13 22:46:18 +0500

    Other user profile updated

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from apps.users.models import User
 from django.shortcuts import get_object_or_404
+from apps.blog.models import Post, VideoPost
 
 
 def user_profile(request, username):
     user = get_object_or_404(User, username=username)
+    posts_count = Post.objects.filter(author=user).count()
+    likes_count = Post.objects.filter(likes__username=user.username).count() + VideoPost.objects.filter(likes__username=user.username).count()
     context = {
-        'cuser': user
+        'cuser': user,
+        'posts_count': posts_count,
+        'likes_count': likes_count
     }
     if user == request.user:
         return render(request, 'user-profile.html', context)
