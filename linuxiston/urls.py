31cf6c796832c2c7bf232f43f5ac8f8133f1408f
sevbo2003 <sevbofx@gmail.com>
2022-04-27 20:55:20 +0500

    url changed

--- linuxiston/urls.py
+++ linuxiston/urls.py
@@ -16,4 +16,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
